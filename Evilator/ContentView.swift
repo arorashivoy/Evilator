3eab5d3b064d6cf02b393c04dc720144b5d4d654
arorashivoy <85881270+arorashivoy@users.noreply.github.com>
2021-11-01 18:08:33 +0530

    Initial Commit

---
+++ Evilator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Evilator
+//
+//  Created by Shivoy Arora on 01/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Evilator/EvilatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  EvilatorApp.swift
+//  Evilator
+//
+//  Created by Shivoy Arora on 01/11/21.
+//
+
+import SwiftUI
+
+@main
+struct EvilatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
